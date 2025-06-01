20ff4c630023ae91ba6f8adc7137c3b7677ccc9f
Abdullah Bdaiwi <bdaiwias@mail.uc.edu>
2025-06-01 16:16:25 -0400

    add a few things to run the demo

--- demo_simulation.m
+++ demo_simulation.m
@@ -1,3 +1,4 @@
+clc; clear; 
 
 load('ref_9vol')
 addpath ./utils
@@ -5,7 +6,8 @@ addpath ./NORDIC_Raw-main
 addpath ./NIfTI_20140122
 addpath ./Denoising-master
 %%% Author:      Xiaoping Wu Xinyu Ye
-
+path = 'D:\Github\Non_local_denoise';
+cd(path);
 
 %% Simulate noisy data with 2 shell b1000 and b2000
 mask0 = dwi0~=0;
@@ -26,15 +28,13 @@ for idx=1: length(levels)
 
     GenerateNoisyDataComplex;
 
-
-
     mask = mask0;
     dwi= dwi0;
     dwi_noisy= dwi0_noisy;
     Sigma= Sigma0;
 
-ARG.temporal_phase = 3;
-ARG.phase_filter_width = 3;
+    ARG.temporal_phase = 3;
+    ARG.phase_filter_width = 3;
     mask = mask(:,:,indz4test);
     dwi = dwi(:,:,indz4test,:);
     dwi_noisy = dwi_noisy(:,:,indz4test,:);
@@ -46,43 +46,33 @@ ARG.phase_filter_width = 3;
 
     KSP2 = im_r;
 
-dwi = dwi(:,:,:,1:nVols);
-%% phase stablization
-DD_phase=0*KSP2;
-
-if           ARG.temporal_phase>0; % Standarad low-pass filtered map
-    for slice=size(KSP2,3):-1:1
-        for n=1:size(KSP2,4);
-            tmp=KSP2(:,:,slice,n);
-            for ndim=[1:2]; tmp=ifftshift(ifft(ifftshift( tmp ,ndim),[],ndim),ndim+0); end
-            [nx, ny, nc, nb] = size(tmp(:,:,:,:,1,1));
-            tmp = bsxfun(@times,tmp,reshape(tukeywin(ny,1).^ARG.phase_filter_width,[1 ny]));
-            tmp = bsxfun(@times,tmp,reshape(tukeywin(nx,1).^ARG.phase_filter_width,[nx 1]));
-            for ndim=[1:2]; tmp=fftshift(fft(fftshift( tmp ,ndim),[],ndim),ndim+0); end
-            DD_phase(:,:,slice,n)=tmp;
+    dwi = dwi(:,:,:,1:nVols);
+    %% phase stablization
+    DD_phase=0*KSP2;
+    
+    if ARG.temporal_phase>0 % Standarad low-pass filtered map
+        for slice=size(KSP2,3):-1:1
+            for n=1:size(KSP2,4)
+                tmp=KSP2(:,:,slice,n);
+                for ndim=[1:2]; tmp=ifftshift(ifft(ifftshift( tmp ,ndim),[],ndim),ndim+0); end
+                [nx, ny, nc, nb] = size(tmp(:,:,:,:,1,1));
+                tmp = bsxfun(@times,tmp,reshape(tukeywin(ny,1).^ARG.phase_filter_width,[1 ny]));
+                tmp = bsxfun(@times,tmp,reshape(tukeywin(nx,1).^ARG.phase_filter_width,[nx 1]));
+                for ndim=[1:2]; tmp=fftshift(fft(fftshift( tmp ,ndim),[],ndim),ndim+0); end
+                DD_phase(:,:,slice,n)=tmp;
+            end
         end
     end
-end
-
-
-
-
-
-
-
-
-
-
-for slice=size(KSP2,3):-1:1
-    for n=1:size(KSP2,4);
-        KSP2(:,:,slice,n)= KSP2(:,:,slice,n).*exp(-i*angle( DD_phase(:,:,slice,n)   ));
+    
+    for slice=size(KSP2,3):-1:1
+        for n=1:size(KSP2,4)
+            KSP2(:,:,slice,n)= KSP2(:,:,slice,n).*exp(-i*angle( DD_phase(:,:,slice,n)   ));
+        end
     end
-end
-
-
-%% gfactor estimation
-KSP2(isnan(KSP2))=0;
-KSP2(isinf(KSP2))=0;
+    
+    %% gfactor estimation
+    KSP2(isnan(KSP2))=0;
+    KSP2(isinf(KSP2))=0;
     for jdx= 1: length(kernelSizes)
         ks= kernelSizes(jdx);
         % ks =5;
@@ -99,60 +89,86 @@ KSP2(isinf(KSP2))=0;
        %     PSNR_sigimag(idx, jdx)= PSNR(Sigma_mppcaimag(mask), Sigma(mask));
         %  PSNR_sig(idx, jdx)= PSNR(Sigma_mppca_realphase(mask), Sigma(mask));
   
-    
     end
 
-for n=1:size(KSP2,4);
-    KSP2(:,:,:,n)= KSP2(:,:,:,n)./ (Sigma_mppca_realphase+eps);
-end
+    for n=1:size(KSP2,4)
+        KSP2(:,:,:,n)= KSP2(:,:,:,n)./ (Sigma_mppca_realphase+eps);
+    end
     
     KSP2(isnan(abs(KSP2)))=0;
     %% step 1 denoise
     ks=5;
-    
-        [denoise_image,Sigma_mppca_realphase22] = denoise_mppca3(real(KSP2),ks);
-
+    [denoise_image,Sigma_mppca_realphase22] = denoise_mppca3(real(KSP2),ks);
      %   [denoise_image2,Sigma_mppca_comphase222] = denoise_mppca3((KSP2),ks);
+        
+    for n=1:size(KSP2,4)
+        denoise_image(:,:,:,n)= denoise_image(:,:,:,n).* Sigma_mppca_realphase;
+    end
+    % 
+    % for n=1:size(KSP2,4);
+    %     denoise_image2(:,:,:,n)= denoise_image2(:,:,:,n).* Sigma_mppca_realphase;
+    % end
     
+        denoise_image(isnan(abs(denoise_image)))=0;
     
-for n=1:size(KSP2,4);
-    denoise_image(:,:,:,n)= denoise_image(:,:,:,n).* Sigma_mppca_realphase;
-end
-% 
-% for n=1:size(KSP2,4);
-%     denoise_image2(:,:,:,n)= denoise_image2(:,:,:,n).* Sigma_mppca_realphase;
-% end
-
-    denoise_image(isnan(abs(denoise_image)))=0;
-
-for n=1:size(KSP2,4);
-    KSP2(:,:,:,n)= KSP2(:,:,:,n)./ (Sigma_mppca_realphase22+eps);
-end
-    
+    for n=1:size(KSP2,4)
+        KSP2(:,:,:,n)= KSP2(:,:,:,n)./ (Sigma_mppca_realphase22+eps);
+    end
+        
     KSP2(isnan(abs(KSP2)))=0;
     %% non local denoise
-nonlocal_com=denoise_nonlocal_svs(KSP2,abs(denoise_image),ks);
-% or mppca
-%nonlocal_com=denoise_nonlocal_mppca(KSP2(:,:,:,:),abs(denoise_image(:,:,:,:)),ks);
-
+    nonlocal_com=denoise_nonlocal_svs(KSP2,abs(denoise_image),ks);
+    % or mppca
+    %nonlocal_com=denoise_nonlocal_mppca(KSP2(:,:,:,:),abs(denoise_image(:,:,:,:)),ks);
     
-for n=1:size(KSP2,4);
-    nonlocal_com(:,:,:,n)= nonlocal_com(:,:,:,n).* Sigma_mppca_realphase22;
-end
+        
+    for n=1:size(KSP2,4)
+        nonlocal_com(:,:,:,n)= nonlocal_com(:,:,:,n).* Sigma_mppca_realphase22;
+    end
+        
+    for n=1:size(KSP2,4)
+        nonlocal_com(:,:,:,n)= nonlocal_com(:,:,:,n).* Sigma_mppca_realphase;
+    end
+        nonlocal_com(isnan(abs(nonlocal_com)))=0;
     
-for n=1:size(KSP2,4);
-    nonlocal_com(:,:,:,n)= nonlocal_com(:,:,:,n).* Sigma_mppca_realphase;
-end
-    nonlocal_com(isnan(abs(nonlocal_com)))=0;
+    foldername = [path, '/denoise/', num2str(level)];
+    if ~isfolder(foldername)
+        mkdir(foldername)
+    end
 
-foldername = ['/denoise/',num2str(level)];
-       str = sprintf('save %s/denoise.mat im_r denoise_image_init nonlocal_com denoise_image Sigma_mppca_realphase22 Sigma_mppca_realphase;',foldername);
-           eval(str)
+    str = sprintf('save %s/denoise.mat im_r denoise_image_init nonlocal_com denoise_image Sigma_mppca_realphase22 Sigma_mppca_realphase;',foldername);
+       eval(str)
 
-      
+          
     
 end
+%% 
+% === Visualization ===
+slice_idx = round(size(im_r, 3) / 2);  % Middle slice
+vol_idx = 1;  % First volume
 
+% Extract and convert to magnitude
+orig_img = abs(im_r(:,:,slice_idx,vol_idx));
+init_denoised = abs(denoise_image_init(:,:,slice_idx,vol_idx));
+final_denoised = abs(nonlocal_com(:,:,slice_idx,vol_idx));
 
+% Plotting
+figure;
+subplot(1,3,1);
+imagesc(orig_img); axis image off;
+colormap gray; colorbar;
+title('Original Noisy');
+
+subplot(1,3,2);
+imagesc(init_denoised); axis image off;
+colormap gray; colorbar;
+title('MPPCA Denoised');
+
+subplot(1,3,3);
+imagesc(final_denoised); axis image off;
+colormap gray; colorbar;
+title('Non-local Denoised');
+
+sgtitle(sprintf('Slice %d - Volume %d', slice_idx, vol_idx));
 
 
